6932caf51fa47631a22152562f979f63893981a7
yumin1209 <55646526+yumin1209@users.noreply.github.com>
2025-06-20 00:29:43 +0900

    Update update_blog.py

--- scripts/update_blog.py
+++ scripts/update_blog.py
@@ -1,5 +1,4 @@
 import feedparser
-import git
 import os
 import re
 
@@ -9,13 +8,6 @@ posts_dir = os.path.join(repo_path, 'velog-posts')
 
 if not os.path.exists(posts_dir):
     os.makedirs(posts_dir)
-
-repo = git.Repo(repo_path)
-
-# git config user.name과 user.email 설정
-with repo.config_writer() as git_config:
-    git_config.set_value('user', 'name', 'yumin1209')
-    git_config.set_value('user', 'email', 'yumin991209@naver.com')
 
 feed = feedparser.parse(rss_url)
 
@@ -29,12 +21,3 @@ for entry in feed.entries:
     if not os.path.exists(file_path):
         with open(file_path, 'w', encoding='utf-8') as f:
             f.write(entry.description)
-
-        repo.git.add(file_path)
-        commit_msg = f'Add post: {entry.title}'
-        repo.git.commit('-m', commit_msg)
-
-try:
-    repo.git.push()
-except Exception as e:
-    print(f'Error pushing to remote: {e}')
